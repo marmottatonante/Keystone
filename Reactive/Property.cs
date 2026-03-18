a920f39dc011fb84308a5e0ce752ca80d6c3f0dc
marmottatonante <tommaso.detommaso@protonmail.com>
2026-03-18 20:49:24 +0100

    Updated Property to Bind and Unbind

--- Reactive/Property.cs
+++ Reactive/Property.cs
@@ -10,15 +10,22 @@ public interface IReadOnlyProperty<T>
 public sealed class Property<T>(T initial) : IReadOnlyProperty<T>
 {
     private T _value = initial;
+    private Action? _unbind;
+
     public event Action? Changing;
     public event Action? Changed;
 
     public T Value
     {
         get => _value;
-        set => Set(value);
+        set
+        {
+            if (_unbind is not null)
+                throw new InvalidOperationException("Property is bound.");
+            Set(value);
+        }
     }
-    
+
     private void Set(T value)
     {
         if (EqualityComparer<T>.Default.Equals(_value, value)) return;
@@ -27,11 +34,21 @@ public sealed class Property<T>(T initial) : IReadOnlyProperty<T>
         Changed?.Invoke();
     }
 
-    public void Bind<B>(IReadOnlyProperty<B> source, Func<B, T> transform) => 
-        source.Changed += () => Value = transform(source.Value);
-
+    public void Bind<B>(IReadOnlyProperty<B> source, Func<B, T> transform)
+    {
+        Unbind();
+        Action handler = () => Set(transform(source.Value));
+        source.Changed += handler;
+        _unbind = () => source.Changed -= handler;
+        Set(transform(source.Value));
+    }
     public void Bind(IReadOnlyProperty<T> source) =>
         Bind(source, value => value);
+    public void Unbind()
+    {
+        _unbind?.Invoke();
+        _unbind = null;
+    }
 
     public IReadOnlyProperty<T> AsReadOnly() => this;
 }
