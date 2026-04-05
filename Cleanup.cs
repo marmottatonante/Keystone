bfc6d667f23e1b3a3fb6b926a467412e1a5f43b8
marmottatonante <tommaso.detommaso@protonmail.com>
2026-04-05 22:43:25 +0200

    Added Cleanup object

---
+++ Cleanup.cs
@@ -0,0 +1,16 @@
+﻿namespace Keystone;
+
+public sealed class Cleanup(Action onDispose) : IDisposable
+{
+    private readonly Action _onDispose = onDispose;
+    private bool _disposed = false;
+    public void Dispose()
+    {
+        if (_disposed) return;
+        _disposed = true;
+        _onDispose();
+    }
+
+    public static Cleanup Merge(params Cleanup[] cleanups) =>
+        new(() => { foreach (var c in cleanups) c.Dispose(); });
+}
